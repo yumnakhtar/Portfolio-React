6644e5c0fc41def53e216cfa0a34449730b6ca27
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-10-24 15:54:03 -0700

    change tab icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import bg_img from './portfolio-bg-pic.jpg';
+import bg_img from './port-bg.jpg';
+// import arrow_down from './arrow-down.jpg';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <img alt="background" className="background" src={bg_img} width="100%"  />
-          <h1 className="App-title">Hi! I'm Yumna</h1>
-          <h3 className="App-title">I'm still working on my portfolio so please be patient with me </h3>
+        <img alt="background" className="background" src={bg_img} width="100%"  />
+        <h1 className="App-title">Hi, I'm Yumna!</h1>
+        <h3 className="App-title">I'm a Full Stack Web Developer </h3>
+        {/* <img alt="arrow-down" className="arrow-down" src={arrow_down} /> */}
       </div>
     );
   }
